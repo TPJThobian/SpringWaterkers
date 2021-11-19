873dbc4f328cc0fe6e07009aa966bde0bd7ebf63
TPJThobian <38331601+TPJThobian@users.noreply.github.com>
2021-11-19 12:15:58 +0100

    First version

---
+++ Spring/src/main/java/com/Waterkers/Spring/SpringwaterkersApplication.java
@@ -0,0 +1,13 @@
+package com.Waterkers.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwaterkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwaterkersApplication.class, args);
+	}
+
+}
